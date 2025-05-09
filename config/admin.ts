f49a95642266fa5bdf511fc9f8de0fefaa5d3d7c
arua <aruawatt@gmail.com>
2025-05-09 15:00:09 +0800

    h

--- config/admin.ts
+++ config/admin.ts
@@ -1,9 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','20c1e6cd6f2f1c081041bfdb9959e17a'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','31b7babce821f42f76d09374e35b37ba'),
   },
   transfer: {
     token: {
--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS','721584f3ecd6272d32cd44167fac7686'),
   },
 });
